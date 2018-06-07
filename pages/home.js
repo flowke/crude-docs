bf84de91f0ee71bb75540130e40fccf0a33f83f3
春田石雨 <flowke@163.com>
2018-06-08 00:07:20 +0800

    init cli

---
+++ pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Name extends Component{
+  constructor(props){
+      super(props);
+  }
+
+  render(){
+    return (
+      <div></div>
+    )
+  }
+}
