486e24b7e00d5cfa0364e9f88a946c36d9bfaf24
flowke <flowke@163.com>
2018-07-02 19:29:33 +0800

    jjk

--- example/SiteCfg.js
+++ example/SiteCfg.js
@@ -4,7 +4,6 @@ module.exports = {
   navLink: [
     {label: '首页', page: 'index'},
     {label: '文档', page: 'docs', type: 'docs'},
-    {label: 'API', page: 'docs', type: 'api'},
     {label: 'GitHub', href: 'https://github.com/flowke/rude-docs'},
   ],
 
