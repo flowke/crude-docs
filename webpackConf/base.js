381cf8fd35bd3b53c83db604c0ea3e90a0f0150f
flowke <flowke@163.com>
2018-07-06 16:09:14 +0800

    fix webpack exclude

--- webpackConf/base.js
+++ webpackConf/base.js
@@ -12,10 +12,7 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        use: 'babel-loader',
-        exclude: [
-          /node_modules/
-        ]
+        use: 'babel-loader'
       },
 
     ]
