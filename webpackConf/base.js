0822e54a233f06fc59df119931748e534ac6658f
flowke <flowke@163.com>
2018-07-12 18:01:07 +0800

    fix exclude babel-loader

--- webpackConf/base.js
+++ webpackConf/base.js
@@ -11,7 +11,7 @@ module.exports = {
         test: /\.js$/,
         use: 'babel-loader',
         exclude: [
-          /node_modules/
+          // /node_modules/
         ],
         include: [
           /crude-docs/
